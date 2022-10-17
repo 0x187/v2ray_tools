e45b661071b9b58b7b8b8f221169fe0229740438
Sr <64494086+0x187@users.noreply.github.com>
2022-10-17 19:06:23 +0330

    Update add_user.sh

--- add_user.sh
+++ add_user.sh
@@ -6,6 +6,6 @@ v2ray=v2ray
 uuid="$($v2ray uuid)"
 
 email="$(grep -F "${1#*@}" "$file_config" | tr -d ' ,"' | sed 's/^email://')"
-[[ "${#email}" == 0 ]] || { echo "user exists" ; exit 1 ;}
+[[ "$(echo -n "$email" | wc -c)" != 0 ]] && { echo "user exists" ; exit 1 ;}
 cp "$file_config" "$file_newconfig"
 cat "$file_config" | jq '(.inbounds[] | select(.protocol=="vmess").settings.clients) += [{"id":"'$uuid'","email":"'$1'","level":1,"alterId":0}]' > "$file_newconfig"
